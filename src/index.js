d459c4f6deb4cbe9815974df8387d1ac50c36136
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-16 15:34:50 -0500

    Added linting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import PropTypes from 'prop-types';
-import Big from 'big.js';
 
 ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
