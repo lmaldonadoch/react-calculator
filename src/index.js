274610b8678d11b6cb8cef98e704d98eb5c26a32
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-16 15:19:15 -0500

    Added depencencies and made sure everything was working correctly

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PropTypes from 'prop-types';
+import Big from 'big.js';
+
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
